d522e922b65d1761e245a14f9975e635219d3bc8
Umair Javed <umairjaved728@gmail.com>
2023-12-16 15:55:39 +0500

    'Nothing'

--- src/app.ts
+++ src/app.ts
@@ -8,29 +8,6 @@ import { routes } from './routes';
 import { AppException } from './common/AppException';
 import { UPLOADS_FOLDER } from './configs/uploadConfig';
 
-const app = express();
-
-//Program.cs
-app.use(express.json());
-app.use(cors());
-
-app.use("/images", express.static(UPLOADS_FOLDER));
-
-app.use(routes)
-app.use((err: { statusCode: number; message: any; }, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppException) {
-        return response.status(err.statusCode).json({
-            status: "error",
-            message: err.message,
-        });
-    }
-
-    console.log(err);
-
-    return response.status(500).json({
-        status: "error",
-        message: "Internal server error",
-    });
 });
 
 const PORT = 2339;
