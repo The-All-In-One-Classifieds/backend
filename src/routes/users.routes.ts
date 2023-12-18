35693b90e9258fbf2936d8de6d7cdd094460800f
Imaan Saleem <imaansaleem12@gmail.com>
2023-12-18 23:46:48 +0500

    emailVerification

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -21,3 +21,4 @@ usersRoutes.get("/me", verifyAuthentication, usersController.show);
 usersRoutes.get('/profile/:id', verifyAuthentication, usersController.getUser);
 usersRoutes.get("/ads", verifyAuthentication, userAdsController.index);
 usersRoutes.put("/update/:id", verifyAuthentication, upload.single("profile_picture"), usersController.update);
+usersRoutes.get("/emailValidation", usersController.validate);
