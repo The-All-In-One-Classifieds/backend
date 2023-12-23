3425ef64ff42a14206f45017431760ef74f9322c
Umair Javed <umairjaved728@gmail.com>
2023-12-23 23:29:25 +0500

    Socket

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,36 @@ import { routes } from './routes';
 import { AppException } from './common/AppException';
 import { UPLOADS_FOLDER } from './configs/uploadConfig';
 
+import http from 'http';
+import { Server } from 'socket.io';
+
 const app = express();
 
 //Program.cs
 app.use(express.json());
 app.use(cors());
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    // Socket.IO options here
+});
+
+console.log("Server Starting...");
+io.on('connection', (socket) => {
+    console.log('A user connected: ', socket.id);
+
+    socket.on('disconnect', (reason) => {
+        console.log(`User disconnected: ${socket.id}, Reason: ${reason}`);
+    });
+
+    // Example of handling a custom chat event
+    socket.on('chat message', (msg) => {
+        console.log(`Message received from ${socket.id}: ${msg}`);
+        io.emit('chat message', "Hi")
+        // You can emit a response or handle the message here
+    });
+});
+
 
 app.use("/images", express.static(UPLOADS_FOLDER));
 
@@ -34,4 +59,8 @@ app.use((err: { statusCode: number; message: any; }, request: Request, response:
 });
 
 const PORT = 2339;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+// Replace this:
+// app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+
+// With this:
+httpServer.listen(PORT, () => console.log(`Server is running on Port ${PORT} along with Socket.IO`));
